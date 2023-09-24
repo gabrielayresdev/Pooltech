3f4459500021dace37429099955cdcc012d9414d
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-09-24 20:04:04 -0300

    Feat: Responsividade do menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
 import styles from "./Header.module.sass";
 import Logo from "../../assets/logo_sph_piscinas.png";
+import React from "react";
 
 const Header = () => {
+  const [visible, setVisible] = React.useState<boolean>();
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img src={Logo} /> SPH Piscinas
       </div>
-      <nav>
+      <div className={styles.openMenu} onClick={() => setVisible(!visible)}>
+        x
+      </div>
+      <nav className={visible ? styles.active : ""}>
         <a href="#sobre">Sobre</a>
         <a href="#servicos">Serviços</a>
         <a href="#depoimentos" className={styles.active}>
