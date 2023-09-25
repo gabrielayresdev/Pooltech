c20b3199be151aa851d64d192d6a6f0f75bb2b67
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-09-25 19:42:22 -0300

    Feat: About section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./About.module.sass";
+
+import Pic1 from "../../assets/Foto 1.png";
+import Pic2 from "../../assets/Foto 2.png";
+import Wave from "../UI/Wave";
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <Wave color="#fff" className={styles.detail} />
+      <div className={styles.container}>
+        <div className={styles.images}>
+          <div className={`${styles.pictureContainer} ${styles.first}`}>
+            <img src={Pic1} />
+          </div>
+          <div className={styles.pictureContainer}>
+            <img src={Pic2} />
+          </div>
+        </div>
+        <div className={styles.text}>
+          <h2 className={styles.title}>Sobre</h2>
+          <p className={styles.paragraph}>
+            A "SPH Piscinas" é uma renomada empresa de manutenção de piscinas
+            que tem se destacado no mercado por muitos anos. Com um firme
+            compromisso com a excelência e a satisfação do cliente, a SPH
+            Piscinas se tornou uma referência na indústria de manutenção de
+            piscinas.
+          </p>
+          <p className={styles.paragraph}>
+            A empresa tem uma longa história de sucesso e crescimento contínuo.
+            Seu sucesso é atribuído à dedicação da equipe em fornecer serviços
+            de alta qualidade e à busca constante pela satisfação do cliente. A
+            SPH Piscinas entende que uma piscina limpa e bem mantida é um ativo
+            valioso para qualquer propriedade, e é por isso que eles se esforçam
+            para oferecer os melhores serviços de manutenção.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Homepage/Home.tsx
+++ src/pages/Homepage/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Services from "../../components/Services/Services";
 import MainSection from "../../components/Main/MainSection";
 import Header from "../../components/Header/Header";
+import About from "../../components/About/About";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <MainSection />
+      <About />
       <Services />
     </div>
   );
