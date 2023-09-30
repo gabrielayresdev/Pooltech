c9d5c8d1641a41f4925c9df07df53e69c626e59d
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-09-30 02:10:16 -0300

    Test: Tests for the form

--- src/tests/Form.test.tsx
+++ src/tests/Form.test.tsx
@@ -1,18 +1,17 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Contact from "../components/Contact/Contact";
+import { sendEmail } from "../services/sendEmail";
+
+jest.mock("../services/sendEmail", () => {
+  return { sendEmail: jest.fn(() => true) };
+});
 
 describe("Form behavior", () => {
-  test("Testando teste", () => {
-    expect(1 + 1).toBe;
-  });
   test("Email is not correct", () => {
     render(<Contact />);
 
     const input = screen.getByTestId("email");
     const button = screen.getByRole("button");
-    const errorMessage = screen.getByTestId(
-      `${input.getAttribute("data-testid") + "_error_message"}`
-    );
 
     fireEvent.change(input, {
       target: {
@@ -22,28 +21,59 @@ describe("Form behavior", () => {
 
     fireEvent.click(button);
 
-    expect(errorMessage).toBeVisible();
+    const errorMessage = screen.getByTestId("email-error-message");
+    expect(errorMessage).toBeInTheDocument();
   });
 
   test("Some mandatory field was not filled in", () => {
     render(<Contact />);
+    const button = screen.getByRole("button");
 
-    const name = screen.getByRole("input[name=name]");
-    const email = screen.getByRole("input[type=email]");
-    const number = screen.getByRole("input[name=number]");
+    fireEvent.click(button);
 
-    const errorMessage = [
-      screen.getByRole(`[data-field=${name.id}]`),
-      screen.getByRole(`[data-field=${email.id}]`),
-      screen.getByRole(`[data-field=${number.id}]`),
-    ];
+    const errorMessage = screen.queryAllByRole("alert");
+    expect(errorMessage).toHaveLength(3);
+  });
+
+  test("Form fields are cleaned when email is submited", () => {
+    render(<Contact />);
+
+    const name = screen.getByTestId("name");
+    const email = screen.getByTestId("email");
+    const number = screen.getByTestId("number");
+    const message = screen.getByTestId("message");
+
+    fireEvent.change(name, {
+      target: {
+        value: "Gabriel Ayres",
+      },
+    });
+    fireEvent.change(email, {
+      target: {
+        value: "gabriel.ar.fort@gmail.com",
+      },
+    });
+    fireEvent.change(number, {
+      target: {
+        value: "5521967213085",
+      },
+    });
+
+    fireEvent.change(message, {
+      target: {
+        value: "Testing form behavior",
+      },
+    });
 
     const button = screen.getByRole("button");
 
     fireEvent.click(button);
 
-    errorMessage.forEach((mes) => {
-      expect(mes).toBeVisible();
-    });
+    const error = screen.queryByRole("alert");
+    expect(error).toBeNull();
+    expect(name.textContent).toBe("");
+    expect(email.textContent).toBe("");
+    expect(number.textContent).toBe("");
+    expect(message.textContent).toBe("");
   });
 });
